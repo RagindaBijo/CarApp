57cfc4962545cd8283abfde9f78cd1680826c6e3
Luka <luka.ivaniadze.2@btu.edu.ge>
2023-01-11 11:27:47 +0400

    twelfth commit

--- app/src/main/java/com/example/carrent/MainActivity.kt
+++ app/src/main/java/com/example/carrent/MainActivity.kt
@@ -4,9 +4,14 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.example.carrent.fragments.ProfileFragment
+import com.example.carrent.fragments.RentalFragment
+import com.example.carrent.fragments.TimeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -18,10 +23,12 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         Log.d(TAG, "onPause")
     }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy")
@@ -32,14 +39,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        //val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        //val fragments: ArrayList<Fragment> = arrayListOf(
+        //   RentalFragment(),
+        //  )
+        // val adapter = ViewPagerAdapter(fragments, this)
+        // viewPager.adapter = adapter
+
+
         supportActionBar?.hide()
         bottomNavigationView = findViewById(R.id.bottomNavView)
         navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
 
     }
-
-
 
 
 }
